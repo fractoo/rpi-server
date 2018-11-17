6fc665e63712787258972dc52ab36e3c4dd19ae7
fractoo <45123090+fractoo@users.noreply.github.com>
2018-11-17 17:34:45 +0200

    test http server

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log("hello world once again");
+const http = require("http");
+var config = {
+  PORT: 80
+};
+
+const server = http.createServer();
+
+server.on("connection", (req, res) => {
+  console.log(req);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<center><p>hello world</p></center>");
+  res.end();
+}).listen(config.PORT);
