c1fbd066e947f257b78e36510a99b5d192990876
fractoo <45123090+fractoo@users.noreply.github.com>
2018-11-17 17:41:30 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ const http = require("http");
 var config = {
   PORT: 80
 };
-console.log("starting...");
 
 const server = http.createServer();
 
@@ -11,4 +10,6 @@ server.on("connection", (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("<center><p>hello world</p></center>");
   res.end();
-}).listen(config.PORT);
+}).listen(config.PORT, () => {
+  console.log(`listening on port ${config.PORT}`);
+});
