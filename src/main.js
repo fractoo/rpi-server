6b90d549c9c60fdeaffee70256c7c1ec4d547a9e
fractoo <45123090+fractoo@users.noreply.github.com>
2018-11-17 18:16:01 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ var config = {
 };
 
 const server = http.createServer((req,res) => {
+  res.writeHead(200, {"Content-Type": "text/html"});
   res.write("<center><p>hello from the other <b>pi</b></p></center>");
   res.end();
 }).listen(config.PORT, () => console.log("started on port " + config.PORT));
